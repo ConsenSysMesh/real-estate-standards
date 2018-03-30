6bf0856a04bfaa7f88e6dbe4ffd6e550354b2492
wilbarnes <31866314+wilbarnes@users.noreply.github.com>
2018-03-30 13:29:25 -0400

    Update MetadataRegistry.sol

--- contracts/MetadataRegistry.sol
+++ contracts/MetadataRegistry.sol
@@ -1,83 +1,94 @@
-pragma solidity 0.4.20;
+pragma solidity ^0.4.21;
 
+contract MetadataRegistry
+{
+	struct Link
+	{
+		string multiaddr;
+		address owner;
 
-/// @title Metadata Registry - A repository storing metadata issued
-///        from any Ethereum account to any spatial unit.
-contract MetadataRegistry {
+	}
 
-    struct Link {
-        string multiaddr; // See https://github.com/multiformats/multiaddr.
-        address owner;
-        // bool permissioned;
-        // bytes32 resultHash;
-    }
+    Link[] numLinks;
 
-    mapping(address => mapping(bytes32 => Link)) public registry;
-    mapping(address => bytes32[]) public subjectKeys;
-    mapping(address => mapping(bytes32 => uint)) public subjectKeysIndex;
+	mapping(address => mapping(bytes32 => Link)) internal registry;
+	mapping(address => bytes32[]) internal subjectKeys;
+	mapping(address => mapping(bytes32 => uint)) internal subjectKeysIndex;
 
-    event LinkSet(
-        address indexed issuer,
-        address indexed subject,
-        bytes32 indexed key,
-        Link value,
-        uint updatedAt);
+	event LinkSet
+	(
+		address indexed issuer,
+		address indexed subject,
+		bytes32 indexed key,
+		Link value,
+		uint updatedAt
+	);
 
-    event LinkRemoved(
-        address indexed issuer,
-        address indexed subject,
-        bytes32 indexed key,
-        uint removedAt);
+	event LinkRemoved
+	(
+		address indexed issuer,
+		address indexed subject,
+		bytes32 indexed key,
+		uint removedAt
+	);
 
-    /// @dev Create or update a link
-    /// @param subject The address for the spatual unit
-    /// @param key The key used to identify the link
-    /// @param multiaddr The multiaddr associated with the link
-    function setLink(address subject, bytes32 key, string multiaddr) public {
-        require(registry[subject][key].length == 0);
-        Link newLink = Link(multiaddr, msg.sender);
-        registry[subject][key] = newLink;
+	function setLink(address _subject, bytes32 _key, string _multiaddr)
+	public
+	{
+		require(registry[_subject][_key].owner == 0);
+		Link memory newLink = Link(_multiaddr, msg.sender);
+		registry[_subject][_key] = newLink;
+		numLinks.length++;
+		
+		uint olen = subjectKeys[_subject].length;
+		subjectKeys[_subject].length++;
+		subjectKeys[_subject][olen] = _key;
+		subjectKeysIndex[_subject][_key] = olen;
 
-        uint olen = subjectKeys[subject].length;
-        subjectKeys[subject].length++;
-        subjectKeys[subject][olen] = key;
-        subjectKeysIndex[subject][key] = olen;
+		emit LinkSet(msg.sender, _subject, _key, newLink, now);		
+	}
 
-        LinkSet(msg.sender, subject, key, newLink, now);
-    }
+	function getLink(address _subject, bytes32 _key)
+	public
+	constant
+	returns (string multiaddr, address owner)
+	{
+		multiaddr = registry[_subject][_key].multiaddr;
+		owner = registry[_subject][_key].owner;
+	}
+	
+	function removeClaim(address _subject, bytes32 _key)
+	public
+	{
+		// asdf
+		require(registry[_subject][_key].owner != 0);
+		require(msg.sender == registry[_subject][_key].owner);
+		delete registry[_subject][_key];
+		numLinks.length--;
+		
+		uint keyIndex = subjectKeysIndex[_subject][_key];
+		
+		// eliminate gap left in array
+		for (uint i = keyIndex; i < subjectKeys[_subject].length-1; i++)
+		{
+			subjectKeys[_subject][i] = subjectKeys[_subject][i+1];
+		}
+		
+		// removal of line below saves 5000 gas
+		// delete subjectKeys[_subject][subjectKeys[_subject].length-1];
+		subjectKeys[_subject].length--;
 
-    /// @dev Allows to retrieve links from other contracts as well as other off-chain interfaces
-    /// @param subject The address to which the link was issued to
-    /// @param key The key used to identify the link
-    function getLink(address subject, bytes32 key) public constant returns(
-        string multiaddr,
-        address owner
-    ) {
-        multiaddr = registry[subject][key].multiaddr;
-        owner = registry[subject][key].owner;
-    }
+		delete subjectKeysIndex[_subject][_key];
 
-    /// @dev Allows to remove a link from the registry.
-    ///      This can only be done by the issuer or the subject of the claim.
-    /// @param subject The address to which the link was issued to
-    /// @param key The key used to identify the link
-    function removeLink(address subject, bytes32 key) public {
-        require(registry[subject][key] != 0);
-        require(msg.sender == registry[subject][key].owner || msg.sender == subject);
-        delete registry[subject][key];
+		emit LinkRemoved(msg.sender, _subject, _key, now);
+	}
 
-        unit keyIndex = subjectKeysIndex[subject][key];
-        delete subjectKeys[subject][keyIndex];
-
-        for (uint i = keyIndex; i<subjectKeys[subject].length-1; i++){
-            subjectKeys[subject][i] = subjectKeys[subject][i+1];
-        }
-        delete subjectKeys[subject][subjectKeys[subject].length-1];
-        subjectKeys[subject].length--;
-
-        delete subjectKeysIndex[subject][key];
-
-        LinkRemoved(msg.sender, subject, key, now);
-    }
-
+	// return number of links attributed to a particular SpatialUnit
+	function getNumLinks()
+	public 
+	constant 
+	returns (uint)
+	{
+		return numLinks.length;
+	}
 }
